9f68ac391a252c45de10e3130dc41858c2effe06
Blaine Edmondson <blaine@ku.edu>
2017-09-01 14:45:07 -0500

    Fixed the removeBack method. Test score is now 95/100

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -96,10 +96,22 @@ bool LinkedList<T>::removeBack()
 	Node<T>* secondintoLast = nullptr;
 	bool isRemoved = false;
 
-	/** TODO 
-		Fix this method
-	*/
+	if(m_front != nullptr)
+	{
+		lastNode = m_front;
 
+		while(lastNode->getNext() != nullptr)
+		{
+			secondintoLast = lastNode;
+			lastNode = lastNode->getNext();
+		}
+
+		delete lastNode;
+		m_size--;
+		lastNode = nullptr;
+		secondintoLast->setNext(nullptr);
+		isRemoved = true;
+	}
 	return(isRemoved);
 }	
 
